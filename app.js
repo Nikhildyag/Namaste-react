3df1a913e9e24e8c63a5e60b7e7620093f83f481
Nikhildyag <157957987+Nikhildyag@users.noreply.github.com>
2024-01-29 11:42:53 +0530

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-const heading = React.createElement("h1", { id: "xyem" }, "this is nikhil");
-console.log(heading);
-const root = ReactDom.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(heading);
