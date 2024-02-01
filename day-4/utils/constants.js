e330687ad1116367470c2dbb9880b280d20d5aa3
Nikhildyag <157957987+Nikhildyag@users.noreply.github.com>
2024-02-01 22:29:05 +0530

    Create constants.js

---
+++ day-4/utils/constants.js
@@ -0,0 +1,8 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://tse2.mm.bing.net/th?id=OIP.eQdIO8NVBLYkG1Q0MCOwSQHaHa&pid=Api&P=0&h=180";
+
+export const API_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.2987502&lng=78.44974309999999&restaurantId=";
