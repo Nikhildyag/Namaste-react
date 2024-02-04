98d2f9cc5dedccd65d6b63eb4b70525b4ed8c52d
Nikhildyag <157957987+Nikhildyag@users.noreply.github.com>
2024-02-04 16:15:25 +0530

    Add files via upload

---
+++ day-6/src/app.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./components/About";
+import Error from "./components/Error";
+import MenuItems from "./components/MenuItems";
+import { lazy, Suspense } from "react";
+
+const Contact = lazy(() => import("./components/Contact"));
+const AppLayout = () => {
+  return (
+    <div className="appLayout">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: (
+          <Suspense fallback={<h1>Nikhil</h1>}>
+            <Contact />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <MenuItems />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
